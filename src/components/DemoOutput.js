e3346c67727156e5b7d2227e778e7d609b305458
Brian <briansmith@rocketcentral.com>
2023-01-31 13:05:09 -0500

    Added gitignore and basic app setup

---
+++ src/components/DemoOutput.js
@@ -0,0 +1,11 @@
+import react, { useState } from 'react';
+import MyParagraph from './MyParagraph';
+
+const DemoOutput = props => {
+    return (
+    <div>Hello World</div>
+    )
+};
+
+
+export default DemoOutput;
